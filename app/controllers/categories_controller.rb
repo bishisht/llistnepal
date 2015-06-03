c3185292419005160764cbfcdb3ab800cd66de6a
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2015-06-03 10:06:41 +0545

    List Nepal. A new Classified site in Ruby on Rails.

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,17 @@
+class CategoriesController < ApplicationController
+  def new
+    @category = Category.new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+end
